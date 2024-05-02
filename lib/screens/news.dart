d7ca09ec26fe317513289bc03a68c010e89f3656
shomyich <220009@astanait.edu.kz>
2024-05-02 20:39:35 +0500

    news were updated

--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -16,7 +16,7 @@ class NewsTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(title),
-      subtitle: Text(content),
+      subtitle: const Text(""),
       leading: Image.network(imageUrl),
       onTap: () {
         Navigator.push(
@@ -101,7 +101,7 @@ class OwnAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text('Inseption News!'),
+      title: const Text('Inseption News!'),
     );
   }
 
@@ -111,21 +111,24 @@ class OwnAppBar extends StatelessWidget implements PreferredSizeWidget {
 
 List<Map<String, String>> newsList = [
   {
-    'title': 'Новость 1',
-    'content': 'Краткое описание новости 1',
+    'title': '13 years later, director Nolan explains the ending of Inception',
+    'content':
+        "The famous American director Christopher Nolan, many years after the release of the film “Inception,” answered a question about its ending, Tengri Life reports with reference to the Men Today portal. If you watched the film Inception (2010), then you probably remember the ending of the film with the spinning top. In the story, the main character Cobb (Leonardo DiCaprio) was reunited with his family, however, it remained unclear whether this really happened in reality or in a dream. There have been debates for many years. According to the film's director, the film has an optimistic ending. “There is a nihilistic view of the ending, but at the same time the main character has decided to move on and live with his children. The ambiguity does not affect the emotional development of the character. This is more of an intellectual work for the audience,” the publication quotes Nolan as saying. ",
     'imageUrl':
-        'https://th.bing.com/th/id/OIP.CjDJ_o3ly_egwYc3GCG3KwHaEK?rs=1&pid=ImgDetMain',
+        'https://tengrinews.kz/userdata/news/2023/news_503241/thumb_m/photo_435609.jpeg',
   },
   {
-    'title': 'Новость 2',
-    'content': 'Краткое описание новости 2',
+    'title': 'Who did Nolan portray as the main characters?',
+    'content':
+        'In his interview with Entertainment Weekly, Nolan explained the principle by which he formed the characters. He said that his team of heroes is a classic example of film production. Cobb is the director, Arthur is the producer, Ariadne is the production designer, Eames is the actor, Saito is the studio, and Fisher is the audience.',
     'imageUrl':
-        'https://th.bing.com/th/id/OIP.CjDJ_o3ly_egwYc3GCG3KwHaEK?rs=1&pid=ImgDetMain',
+        'https://kinowar.com/wp-content/uploads/2020/04/%D0%94%D0%B8%D0%9A%D0%B0%D0%BF%D1%80%D0%B8%D0%BE-%D1%89%D1%83%D1%80%D0%B8%D1%82%D1%81%D1%8F-%D0%B2-%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D0%B5.png',
   },
   {
-    'title': 'Новость 3',
-    'content': 'Краткое описание новости 3',
+    'title': 'Hidden meaning in names',
+    'content':
+        'If you take the capital letters of the main characters in English Dom, Robert, Eames, Arthur, Mal and Saito, you get the word "Dreams". And if you add Peter, Ariadne, and Yusuf, you get the word “Pay”. Taken together, this would most likely be better translated as “Dreams to order.” However, “buying dreams” also reflects the idea that Nolan wanted to show, referring to the occupation of the main characters.',
     'imageUrl':
-        'https://th.bing.com/th/id/OIP.CjDJ_o3ly_egwYc3GCG3KwHaEK?rs=1&pid=ImgDetMain',
+        'https://kinowar.com/wp-content/uploads/2020/04/%D0%9B%D0%B5%D0%B2%D0%B8%D1%82%D1%82-%D0%A1%D1%86%D0%B5%D0%BD%D0%B0-%D0%B2-%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%B5.jpg',
   },
 ];
