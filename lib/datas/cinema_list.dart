b981cdd12e07cd2952880022b0265c8c331d8079
nurlykhan.kakpan <nurlykhan.kakpan@hardcode.kz>
2024-05-03 13:40:14 +0500

    feedbacks

---
+++ lib/datas/cinema_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_movie/models/cinema.dart';
+
+final List<Cinema> cinemaList = [
+  Cinema(name: 'Chaplin MEGA Silk Way', address: 'Астана, ТРЦ «MEGA Silk Way»\n (территория EXPO), 2-й этаж', phone: '87770760052', image: 'https://cdn.kino.kz/cinema/146/g800x534_8.jpg'),
+  Cinema(name: 'Eurasia Cinema7', address: 'Астана, ул. Петрова, д.24, ТЦ "Евразия"', phone: '87761826768', image: 'https://cdn.kino.kz/cinema/146/g800x534_8.jpg'),
+  Cinema(name: 'Kinopark 8 Keruen', address: 'Астана, ТРЦ "Керуен Сити", 4-й этаж', phone: '87770760052', image: 'https://cdn.kino.kz/cinema/146/g800x534_8.jpg'),
+  Cinema(name: 'Kinopark 8 Moskva', address: 'Астана, ТРЦ "Москва", 3-й этаж', phone: '87770760052', image: 'https://cdn.kino.kz/cinema/146/g800x534_8.jpg'),
+  Cinema(name: 'Kinopark 8 Saryarka', address: 'Астана, ТРЦ "Сарыарка", 3-й этаж', phone: '87770760052', image: 'https://cdn.kino.kz/cinema/146/g800x534_8.jpg'),
+];
